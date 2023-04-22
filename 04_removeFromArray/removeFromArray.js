0561937c93cb4102ee337c605ce5bf05d525e319
freddyal <freddyal0x01@gmail.com>
2023-04-21 21:33:24 -0400

    Finished JS exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const newArr = [];
 
+    arr.forEach((index) => {
+        if(!args.includes(index)) {
+            newArr.push(index);
+        }
+    });
+
+    return newArr;
 };
 
 // Do not edit below this line
