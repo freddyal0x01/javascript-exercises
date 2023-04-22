bcb86f962f659db781bf4a52a06d293109f1a194
freddyal <freddyal0x01@gmail.com>
2023-04-21 21:33:16 -0400

    Finished JS exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num > 0) return "Error";
+    let printString = ""
+    for (let i = 0; i < num; i++) {
+      printString += str;
+    }
+    return printString;
 };
 
 // Do not edit below this line
