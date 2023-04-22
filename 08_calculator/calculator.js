28d7a6ef10470dceb7192f9175725306b9d4e3e9
freddyal <freddyal0x01@gmail.com>
2023-04-21 23:15:58 -0400

    Finished JS Assignment 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(...arr) {
+	return arr.length ? arr.reduce((acc, curr) => acc * curr) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	// If the number is less than 0, reject it.
+  if (num < 0) {
+    return -1;
+  } 
+  // If the number is 0, its factorial is 1.
+  else if (num === 0) {
+    return 1;
+  }
+  // Otherwise, call the recursive procedure again
+  else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
