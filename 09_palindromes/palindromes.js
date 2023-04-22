8bbc8115600beffc3f786ba01bd24f868be1aa55
freddyal <freddyal0x01@gmail.com>
2023-04-21 23:16:02 -0400

    Finished JS Assignment 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    testWord = str.toLowerCase().replace(/[^a-z]/g, "");
+    return testWord.split("").reverse().join("") === testWord;
 };
 
 // Do not edit below this line
