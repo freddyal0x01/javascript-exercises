afb8a30ee5f997fdff51d33361fd582811949eee
freddyal <freddyal0x01@gmail.com>
2023-04-21 21:33:30 -0400

    Finished JS exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "Error!";
+    if (min < 0 || max < 0) return "Error!";
 
+    if (min > max) {
+        const temp = max;
+        min = max;
+        max = temp;
+    }
+
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
